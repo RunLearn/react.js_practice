07c41302d8403bf89f02c0bf9330a869fca95d63
RunLearn <wg7363@naver.com>
2021-10-28 13:42:40 +0900

    JavaScript Tutorial 2.4 Variables

--- sample/script.js
+++ sample/script.js
@@ -1,1 +1,26 @@
-alert("Hi, I'm Javascript")
+"use strict";
+
+//변수
+let message= 'Hello';
+alert(message)
+let num1= 1;
+let num2= 2;
+let num3= 3;
+let result = num1 + num2 + num3;
+alert(result);
+
+//상수
+const myBirthDay='1998.10.27';
+alert(myBirthDay);
+
+//예제
+let admin;
+let name = 'John';
+admin = name;
+alert(admin);
+
+//예제
+let ourPlanetName ='earth';
+let currentUserName = 'John';
+alert(ourPlanetName);
+alert(currentUserName);
